f5279945bd4f865849ce6edc81566e06f23c69d0
bduong0929 <bao.duong@revature.com>
2023-05-26 23:36:33 -0400

    added unit testing and mocking to user service

--- src/main/java/com/revature/yolp/YolpApplication.java
+++ src/main/java/com/revature/yolp/YolpApplication.java
@@ -2,6 +2,7 @@ package com.revature.yolp;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import com.revature.yolp.services.RouterService;
@@ -23,8 +24,9 @@ public class YolpApplication {
   public static void main(String args[]) throws ClassNotFoundException, IOException, SQLException {
     Scanner scan = new Scanner(System.in);
 
+    Session session = new Session();
     // Create a new RouterService with a Session
-    RouterService router = new RouterService(new Session());
+    RouterService router = new RouterService(session);
 
     // Navigate to the "/home" route using the router and scanner
     router.navigate("/home", scan);
